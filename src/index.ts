5733d6218cbc007c5a0aa92fa6ff63e9f45c1c6b
Tom Palmer <tjpalmer@tjpalmer.com>
2018-03-16 18:42:44 -0700

    Handle resize

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,52 @@ import {
 } from 'three';
 
 addEventListener('load', () => {
-  let display = document.querySelector('.display') as HTMLElement;
-  let renderer = new WebGLRenderer({canvas: display.querySelector('canvas')!});
-  let size = display.getBoundingClientRect();
-  renderer.setSize(size.width, size.height);
-
-  let scene = new Scene();
-
-  let geometry = new BoxGeometry(0.2, 0.2, 0.2);
-  let material = new MeshNormalMaterial();
-  let mesh = new Mesh(geometry, material);
-  scene.add(mesh);
-
-  let camera = new PerspectiveCamera(70, size.width / size.height, 0.01, 10);
-  camera.position.z = 1;
-
-  renderer.render(scene, camera);
+  new App();
 });
+
+class App {
+
+  constructor() {
+    let {camera, scene} = this;
+    // Display.
+    this.display = document.querySelector('.display') as HTMLElement;
+    this.renderer = new WebGLRenderer({
+      canvas: this.display.querySelector('canvas')!,
+    });
+    // Scene.
+    let geometry = new BoxGeometry(0.2, 0.2, 0.2);
+    let material = new MeshNormalMaterial();
+    let mesh = new Mesh(geometry, material);
+    scene.add(mesh);
+    // Camera.
+    camera.position.z = 1;
+    this.resize();
+    addEventListener('resize', this.resize);
+  }
+
+  display: HTMLElement;
+
+  camera = new PerspectiveCamera(70, 1, 0.01, 10);
+
+  render() {
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  renderer: WebGLRenderer;
+
+  resize = () => {
+    let {camera, renderer} = this;
+    let {style} = renderer.domElement;
+    renderer.setSize(0, 0);
+    setTimeout(() => {
+      let {height, width} = this.display.getBoundingClientRect();
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      this.render();
+    }, 0);
+  };
+
+  scene = new Scene();
+
+}
