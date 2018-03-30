fa3f26c7b8b623934c6988fc9b4fc14df9bbcd83
Tom <tjpalmer@tjpalmer.com>
2018-03-30 07:00:18 -0700

    Size 0 bone at end

--- src/body.ts
+++ src/body.ts
@@ -92,7 +92,10 @@ export class Creature extends Object3D {
     // Still broken typing here on bones.
     let bones = [] as EditorBone[];
     let prevBone: EditorBone | undefined;
-    let positions = [2, 1.75, 1.625, 1.5, 1.375, 1.25, 1];
+    // Last bone length 0 because for some reason the last doesn't like to
+    // rotate when dragged by others?
+    // TODO Just a final size 0 physics body, not semantic bone.
+    let positions = [2, 1.75, 1.625, 1.5, 1.375, 1.25, 1, 1];
     positions.slice(1).forEach((y, i) => {
       let length = positions[i] - y;
       let bone = new EditorBone(length);
